fc8f561dd92d58086b9b2e92aec4c1363ad92942
Stefan Schubert <stefan.schubert@prodyna.com>
2022-07-03 23:21:40 +0200

    Adds test dialog

--- lib/widgets/meal_preview.dart
+++ lib/widgets/meal_preview.dart
@@ -6,7 +6,7 @@ class MealPreview extends StatefulWidget {
   const MealPreview({
     Key? key,
     required this.meal
-  }) : super(key:key);
+  }) : super(key: key);
 
   final Meal meal;
 
@@ -20,32 +20,66 @@ class _MealPreviewState extends State<MealPreview> {
     final _isFavorite = widget.meal.id != null;
     return Center(
         child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                    child: Image.asset(
-                      'images/generic_meal.jpg',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    )
-                  ),
-                  Flexible(
-                    child: ListTile(
-                      title: Text(widget.meal.name, style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.5)),
-                      trailing: IconButton(onPressed: (){}, icon: Icon(_isFavorite ? Icons.favorite_border : Icons.favorite), color: Colors.red),
-                    )
-                  ),
-                ],
-              )
-            ],
-          )
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                        child: Image.asset(
+                          'images/generic_meal.jpg',
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        )
+                    ),
+                    Flexible(
+                        child: ListTile(
+                          title: Text(widget.meal.name, style: DefaultTextStyle
+                              .of(context)
+                              .style
+                              .apply(fontSizeFactor: 1.5)),
+                          trailing: IconButton(
+                              onPressed: _isFavorite? () => _saveMeal(widget.meal): () => {},
+                              icon: Icon(
+                                  _isFavorite ? Icons.favorite_border : Icons
+                                      .favorite),
+                              color: Colors.red),
+                        )
+                    ),
+                  ],
+                )
+              ],
+            )
         )
-      );
+    );
+  }
+
+  Future<void> _saveMeal(Meal meal) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text("Give some more information for this meal", style: DefaultTextStyle
+                .of(context)
+                .style
+                .apply(fontSizeFactor: 2)),
+            children: [
+              Text(meal.name),
+              Text(meal.name),
+              Text(meal.name),
+              Padding(
+                  padding: const EdgeInsets.only(left:10, right: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20), minimumSize: Size(80, 50)),
+                    onPressed: () {},
+                    child: const Text('Generate Shopping-List'),
+                  )
+              ),
+            ],
+          );
+        });
   }
 }
